ba4f00283dc1acf18a8a7e178c914a3bd30bad5c
Milan Pavic <milanpavice@gmail.com>
2024-06-15 14:58:18 +0200

    test change

--- apps/web/src/app/page.tsx
+++ apps/web/src/app/page.tsx
@@ -67,7 +67,7 @@ export default function Page(): JSX.Element {
               src="/vercel.svg"
               width={100}
             />
-            proba
+            probatest
           </a>
         </div>
       </div>
